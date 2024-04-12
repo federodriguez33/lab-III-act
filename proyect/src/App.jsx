c25f2e86164a112a4baa17d4e761aafad8f3a67f
federodriguez33 <federicosebastianrodriguez@gmail.com>
2024-04-11 21:10:06 -0300

    commit2
    commit2

--- proyect/src/App.jsx
+++ proyect/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
+import Beers from "./component/beers/Beers";
 
 function App() {
-  
-  
-  return (
-    
-  <>
 
-  </>
+
+  return (
+
+    <>
+      <h2>Cerveza: {beerName}</h2>
+      <h3>Estilo {beerStyle}</h3>
+      <p>${price * 1000}</p>
+      <p>{available}</p>
+    </>
 
   )
 
