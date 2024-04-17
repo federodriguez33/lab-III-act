dbbce70556c8635ae52d26255a455c34fc3c499f
federodriguez33 <federicosebastianrodriguez@gmail.com>
2024-04-17 12:48:36 -0300

    inicio ejercico
    inicio ejercico

--- proyect/src/App.jsx
+++ proyect/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import Table from "./component/table/Table";
 
 function App() {
   
+  const netIncomes = [{brand: "McDonalds", income: 1291283}, {brand: "Burger King", income: 1927361}, {brand: "KFC", income: 1098463}];
   
   return (
     
-  <> 
+  <>
 
+    <Table netIncomes={netIncomes}></Table>
   </>
 
   )
---
+++ proyect/src/component/table/Table.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+    
+const Table = (netIncomes) => {
+
+    netIncomes.map()
+    
+    return(
+
+        <table>
+            <tr>
+                <td>{}</td>
+                <td>{}</td>
+            </tr>
+        </table>
+
+    )
+}
+
+Table.propTypes = {
+    netIncomes: PropTypes.array
+}
+
+export default Table
